2fe366fbc104ce27732c04489615a9f0bc898d9d
123321abccba1 <47651072+123321abccba1@users.noreply.github.com>
2020-03-05 12:33:05 +0800

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,8 @@
+// array of Monitor-specific API keys or Main API key to list all monitors
+var __apiKeys = [
+	'u809008-a35f473c7728f25fd577e300', //Community
+
+];
+
+// refresh interval (in seconds)
+var __refresh = 180;
